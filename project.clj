d5c5b3107ccc3846f789ea82dc6c65c0398da946
Marcos <marcoslamuria@gmail.com>
2015-08-08 01:03:00 +0100

    update project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-mailgun "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure Wrapper to Mailgun API"
+  :url "https://github.com/lamuria/clj-mailgun"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
